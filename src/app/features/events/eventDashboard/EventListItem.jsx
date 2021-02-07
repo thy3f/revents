dbe5a7735fe3d76ef301c2e2156e984be044508e
thy3f <andrei.galan@gmail.com>
2021-02-07 17:40:07 +0200

    End of section 4

--- src/app/features/events/eventDashboard/EventListItem.jsx
+++ src/app/features/events/eventDashboard/EventListItem.jsx
@@ -1,7 +1,7 @@
 import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
-export default function EventListItem({event}) {
+export default function EventListItem({ event, selectEvent, deleteEvent }) {
 	return (
 		<Segment.Group>
 			<Segment>
@@ -19,21 +19,32 @@ export default function EventListItem({event}) {
 			</Segment>
 			<Segment>
 				<span>
-					<Icon name='clock'/> {event.date}
-					<Icon name='marker'/> {event.venue}
+					<Icon name='clock' /> {event.date}
+					<Icon name='marker' /> {event.venue}
 				</span>
 			</Segment>
 			<Segment secondary>
 				<List horizontal>
-					{event.attendees.map(attendee => (
+					{event.attendees.map((attendee) => (
 						<EventListAttendee key={attendee.id} attendee={attendee} />
 					))}
 				</List>
 			</Segment>
 			<Segment clearing>
 				<div>{event.description}</div>
-				<Button color='teal' floated='right' content='View' />
+				<Button
+					onClick={() => deleteEvent(event.id)}
+					color='red'
+					floated='right'
+					content='Delete'
+				/>
+				<Button
+					onClick={() => selectEvent(event)}
+					color='teal'
+					floated='right'
+					content='View'
+				/>
 			</Segment>
 		</Segment.Group>
-	)
-};
+	);
+}
