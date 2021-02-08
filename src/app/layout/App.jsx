04cf564204e31124b02c57a1380a1a37ff4127a2
thy3f <andrei.galan@gmail.com>
2021-02-08 11:01:25 +0200

    End of section 5

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from "react";
+import { Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import EventDashboards from "../features/events/eventDashboard/EventDashboards";
 import NavBar from "../features/nav/NavBar";
+import HomePage from "../features/home/HomePage";
+import EventDetailedPage from "../features/events/eventDetailed/EventDetailedPage";
+import EventForm from "../features/events/eventForm/EventForm";
 
 function App() {
-	const [formOpen, setFormOpen] = useState(false);
-	const [selectedEvent, setSelectedEvent] = useState(null);
-
-	function handleSelectEvent(event) {
-		setSelectedEvent(event);
-		setFormOpen(true);
-	}
-
-	function handleCreateFormOpen() {
-		setSelectedEvent(null);
-		setFormOpen(true);
-	}
-
 	return (
 		<>
-			<NavBar setFormOpen={handleCreateFormOpen} />
-			<Container className='main'>
-				<EventDashboards
-					formOpen={formOpen}
-					setFormOpen={setFormOpen}
-					selectEvent={handleSelectEvent}
-					selectedEvent={selectedEvent}
-				/>
-			</Container>
+			<Route exact path='/' component={HomePage} />
+			<Route
+				path={"/(.+)"}
+				render={() => (
+					<>
+						<NavBar />
+						<Container className='main'>
+							<Route exact path='/events' component={EventDashboards} />
+							<Route path='/events/:id' component={EventDetailedPage} />
+							<Route
+								path={["/createEvent", "/manage/:id"]}
+								component={EventForm}
+							/>
+						</Container>
+					</>
+				)}
+			/>
 		</>
 	);
 }
